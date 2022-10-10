116f1950473b0994a0212a60f8efaa72e54d5d0a
Prof. Dr. Cesar Augusto Guzman Alvarez <cesar.guzman@visionspace.com>
2022-10-10 21:04:03 +0200

    Preparing script for the EM session

---
+++ results/em/exec_test_train_classifier_models_thread.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+echo "Running test train classifier models with thread"
+
+# experimenter id 213
+
+# datasetId 1
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 1, \"algorithm\" : \"LogisticRegressionDCD\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 1, \"algorithm\" : \"LinearSGD\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 1, \"algorithm\" : \"PassiveAggressive\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 1, \"algorithm\" : \"StochasticMultinomialLogisticRegression\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 1, \"algorithm\" : \"ALMA2\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 1, \"algorithm\" : \"NewGLMNET\", \"thread\" : true }" &
+
+
+# dataset id 2
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 2, \"algorithm\" : \"LogisticRegressionDCD\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 2, \"algorithm\" : \"LinearSGD\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 2, \"algorithm\" : \"PassiveAggressive\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 2, \"algorithm\" : \"StochasticMultinomialLogisticRegression\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 2, \"algorithm\" : \"ALMA2\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 2, \"algorithm\" : \"NewGLMNET\", \"thread\" : true }" &
+
+
+# dataset id 3
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 3, \"algorithm\" : \"LogisticRegressionDCD\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 3, \"algorithm\" : \"LinearSGD\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 3, \"algorithm\" : \"PassiveAggressive\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 3, \"algorithm\" : \"StochasticMultinomialLogisticRegression\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 3, \"algorithm\" : \"ALMA2\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 3, \"algorithm\" : \"NewGLMNET\", \"thread\" : true }" &
+
+
+# dataset id 4
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 4, \"algorithm\" : \"LogisticRegressionDCD\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 4, \"algorithm\" : \"LinearSGD\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 4, \"algorithm\" : \"PassiveAggressive\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 4, \"algorithm\" : \"StochasticMultinomialLogisticRegression\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 4, \"algorithm\" : \"ALMA2\", \"thread\" : true }" &
+time java -jar saasy-ml-app-api-client-0.1.1.jar -server http://$1/api/v1/training/classifier/ -payload "{ \"expId\": 213, \"datasetId\": 4, \"algorithm\" : \"NewGLMNET\", \"thread\" : true }" &
+
+
